a8aed497e26b9393101af957b2449dd64ba83acc
janewuwz <janewuwz@gmail.com>
2018-03-26 15:08:24 +0800

    delete extra

--- src/compileEle.js
+++ src/compileEle.js
@@ -1,4 +1,3 @@
-import generateTree from './domTree'
 
 export function realRender (tree) {
   Object.keys(tree).forEach((item, index) => {
@@ -40,56 +39,4 @@ export function realRender (tree) {
       })
     }
   })
-}
-
-export function parseDomTree (tree, diffTarget) {
-  if (diffTarget !== undefined && document.getElementById(diffTarget.id)) {
-    const targetDom = document.getElementById(diffTarget.id)
-    targetDom.firstChild.textContent = diffTarget.content
-    return
-  }
-  Object.keys(tree).forEach((item, index) => {
-    const value = tree[item]
-    const ele = document.createElement(value.type)
-    ele.id = item
-    value.eventType && ele.addEventListener(value.eventType, value.event)
-    ele.textContent = value.content
-    if (value.eventHandler) {
-      ele.addEventListener(value.eventType, value.eventHandler)
-    }
-    const ParentDom = document.getElementById(value.parent)
-    ParentDom.appendChild(ele)
-    if (value.child.length > 0) {
-      value.child.forEach((sub, count) => {
-        parseDomTree(sub)
-      })
-    }
-  })
-}
-
-let states = []
-
-export function diff (newState, click1) {
-  // console.log(newTree)
-  states.push(newState)
-
-  const diffTarget = {
-    id: '1',
-    content: newState}
-  const oldTree = generateTree(diffTarget, click1, 'click')
-  // if (prevState[0].cur === prevState[1].cur) {
-  //   return
-  // }
-  // walkTree(oldTree, target)
-  // const targetObj = init[0]
-  // targetObj.eventType = 'click'
-  // targetObj.eventHandler = clickEvent
-  // if ((JSON.stringify(newTree) === JSON.stringify(oldTree)) && document.getElementById('root').childElementCount > 0) {
-  //   return
-  // }
-  // if (init[0]) {
-  //   parseDomTree(null, init[0], clickEvent)
-  //   return
-  // }
-  parseDomTree(oldTree, diffTarget)
 }
--- src/wz.js
+++ src/wz.js
@@ -1,23 +1,19 @@
 import {render} from './render'
 import uuid from 'node-uuid'
-import * as _ from 'lodash'
 
-function updateState (self) {
-  return new Promise((resolve, reject) => {
-    self.batchedState.forEach(element => {
-      self.state = {
-        ...self.state,
-        ...element
-      }
-    })
-    render(document.getElementById('root'), self)
-  })
-}
-const root = document.getElementById('root')
-let parents = [root]
-let childs = []
+// function updateState (self) {
+//   return new Promise((resolve, reject) => {
+//     self.batchedState.forEach(element => {
+//       self.state = {
+//         ...self.state,
+//         ...element
+//       }
+//     })
+//     render(document.getElementById('root'), self)
+//   })
+// }
+
 window.tree = {}
-var a = 0
 window.temp = []
 export function makeElement () {
   const vid = uuid.v4()
@@ -53,70 +49,6 @@ export function makeElement () {
     })
   }
   return window.tree[vid]
-
-  // if (other.length === 1 && typeof other[0] !== 'function') {
-  //   tree[vid].
-  // }
-}
-
-// export function makeElement () {
-//   const type = arguments[0]
-//   const dom = document.createElement(type)
-//   if (childs.length > 0) {
-//     dom.appendChild(childs.pop())
-//   }
-//   const attr = arguments[1]
-//   if (attr !== null) {
-//     Object.entries(attr).map(item => {
-//       console.log(typeof item[1])
-//       if (typeof item[1] === 'function') {
-//         dom[item[0]] = item[1]
-//       } else {
-//         dom.setAttribute(item[0], item[1])
-//       }
-//     })
-//   }
-//   childs.push(dom)
-//   const other = [...arguments].slice(2)
-//   if (other.length === 1 && typeof other[0] === 'string') {
-//     const text = document.createTextNode(other[0])
-//     dom.appendChild(text)
-//   }
-//   if (other.length > 1 && other[0] !== 'string') {
-//     other.map(item => {
-//       dom.appendChild(item)
-//       parents.push(dom)
-//     })
-//   }
-//   if (type) {
-//     return dom
-//   }
-// }
-
-// export function makeElement (type, attr, ...args) {
-//   const content = args[0]
-//   return parse(content, type, attr)
-// }
-
-function parse (content, type, attr) {
-  const dom = document.createElement(type)
-  if (childs.length > 0) {
-    dom.appendChild(childs.pop())
-  }
-  if (attr !== null) {
-    Object.entries(attr).map(item => {
-      dom[item[0]] = item[1]
-    })
-  }
-  childs.push(dom)
-  if (typeof content === 'string') {
-    const text = document.createTextNode(content)
-    dom.appendChild(text)
-  } else {
-    parents.push(dom)
-  }
-
-  return dom
 }
 
 window.prevTree = {}
@@ -142,7 +74,6 @@ export class Component {
         ...this.state,
         ...newState
       }
-      // console.log(this.state)
       render(document.getElementById('root'), this)
       // updateState(this).then(cb && cb())
     }
