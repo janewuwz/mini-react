a91cf69d753d4a1c13aaf45d382881c6c24fe5f4
janewuwz <janewuwz@gmail.com>
2018-03-29 11:20:00 +0800

    count

---
+++ src/count.js
@@ -0,0 +1,32 @@
+import {Component, makeElement} from './wz'
+import {render} from './render'
+
+class Instance extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 5
+    }
+  }
+
+  click1 = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  click2 = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render () {
+    /**
+     * <div>
+     *  <span><h1 name='haha'>h tag</h1></span>
+     *  <span name='baz'>{count}</span>
+     * </div>
+     */
+    const {count} = this.state
+    return makeElement('div', { id: 'foo' }, makeElement('span', {name: 'bar'}, makeElement('h2', {name: 'haha', onclick: this.click1}, 'increase')), makeElement('span', {name: 'baz'}, count), makeElement('h2', {onclick: this.click2}, 'decrease'))
+  }
+}
+
+render(document.getElementById('root'), Instance)
