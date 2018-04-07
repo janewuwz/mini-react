759cfc8469c1a8e2e2dfb4bffa01bae2f4e00013
jane wu <janewuwz@gmail.com>
2018-04-07 11:37:48 +0800

    Develop lifecycle (#4)
    * sort node
    
    * refactor
    
    * patch sth
    
    * fix state bug
    
    * add style
    
    * refactor
    
    * del extra code
    
    * patch sth
    
    * patch sth
    
    * funcname
    
    * lifecycle
    
    * del extra
    
    * patch sth

--- src/wz.js
+++ src/wz.js
@@ -1,15 +1,4 @@
 import {render} from './render'
-// function updateState (self) {
-//   return new Promise((resolve, reject) => {
-//     self.batchedState.forEach(element => {
-//       self.state = {
-//         ...self.state,
-//         ...element
-//       }
-//     })
-//     render(document.getElementById('root'), self)
-//   })
-// }
 
 window.tree = {}
 window.temp = []
@@ -108,22 +97,11 @@ export class Component {
     this.batchedState = []
     this.batchedCb = []
     this.setState = (newState, cb) => {
-      // window.prevTree = {...window.}
-      // this.batchedState.push(newState)
-      // this.batched.forEach(item => {
-      //   this.state = {
-      //     ...this.state,
-      //     ...item.newState
-      //   }
-      //   item.cb && item.cb()
-      //   render(document.getElementById('root'), this)
-      // })
       this.state = {
         ...this.state,
         ...newState
       }
       render(document.getElementById('root'), this)
-      // updateState(this).then(cb && cb())
     }
     // this.render = function () {
     //   console.log('ren')
@@ -142,4 +120,5 @@ export class Component {
   ComponentWillUnMount () {
 
   }
+  render () {}
 }
